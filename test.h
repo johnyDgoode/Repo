c94f6f45e168de424664d1979715940fdbc97eda
Kenta Saito <ptanptanptan@gmail.com>
2011-12-20 18:25:25 +0900

    aaa
    aaa

---
+++ test.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Adlib
+//
+//  Created by Kenta Saito on 11/12/19.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic,retain) UIWindow *window;
+
+@end
