5c83c17ba09632246f5eb76f15d2584c724c7c5a
Kenta Saito <ptanptanptan@gmail.com>
2011-12-20 19:02:55 +0900

    rrr
    rrr

---
+++ test3.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Adlib
+//
+//  Created by Kenta Saito on 11/12/19.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic,retain) UIWindow *window;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+@property (nonatomic,retain) UIWindow *window2;
+
+
+
+@end
